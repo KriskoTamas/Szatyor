31b0f11fe035a5705a1829f511ff325842429a64
Tamás Kriskó <tomi.krisko@gmail.com>
2022-04-09 22:15:42 +0200

    Tweaked UI

--- Assets/Scripts/Gesture.cs
+++ Assets/Scripts/Gesture.cs
@@ -33,13 +33,13 @@ public class Gesture : MonoBehaviour
         if (sensor.IsAvailable)
         {
             Game.kinectConnected = true;
-            UIManager.kinectInfoText.text = "A Kinect csatlakoztatva van.";
+            UIManager.kinectInfoText.text = "A Kinect csatlakoztatva van";
             UIManager.kinectInfoText.color = new Color(166f/255f, 255f/255f, 77f/255f);
         }
         else
         {
             Game.kinectConnected = false;
-            UIManager.kinectInfoText.text = "A Kinect nincsen csatlakoztatva.";
+            UIManager.kinectInfoText.text = "A Kinect nincsen csatlakoztatva";
             UIManager.kinectInfoText.color = new Color(255f/255f, 128f/255f, 128f/255f);
         }
         // print("sensor available: " + sensor.IsAvailable);
